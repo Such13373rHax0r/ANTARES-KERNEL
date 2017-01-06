c8fa5df9b8797a45292c4bfb95012f20a965b871
Simon Kadesh <Sgkadesh@icloud.com>
2017-01-06 17:22:42 -0500

    t

--- kernel.asm
+++ kernel.asm
@@ -17,32 +17,19 @@ stack_bottom:
 stack_top:
 
 .section .data
-_gdt:
-.quad 0x0000000000000000
-.quad 0x00CF9A000000FFFF
-.quad 0x00CF92000000FFFF
-.quad 0x00CFFA000000FFFF
-.quad 0x00CFF2000000FFFF
-_gdtr:
-.word 0x140
-.long _gdt
 
 
 .section .text
 .global _start
 .type _start, @function
 _start:
-
-
-	lgdt _gdtr
-	_code:
 	mov $0x10, %ax
 	mov %ax, %ds
 	mov %ax, %fs
 	mov %ax, %es
 	mov %ax, %gs
 	mov %ax, %ss
-
+	
 	mov $stack_top, %esp
 	call kmain
 
